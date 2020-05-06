3e18443698029867c24f17b4660e9fdfd181081b
Mayu Imanaka <imamayu.1215@gmail.com>
2020-05-05 22:42:18 -0700

    Updates

---
+++ precache-manifest.0ace90f57f3385cb93971859e0edbba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d316f474164097722de11af0e54764f",
+    "url": "/React-snap-shot/index.html"
+  },
+  {
+    "revision": "28c18e774b8f3d966d80",
+    "url": "/React-snap-shot/static/css/main.632f5c3f.chunk.css"
+  },
+  {
+    "revision": "f94104475ddbbd055dec",
+    "url": "/React-snap-shot/static/js/2.56c6a2b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-snap-shot/static/js/2.56c6a2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c18e774b8f3d966d80",
+    "url": "/React-snap-shot/static/js/main.c2ea198b.chunk.js"
+  },
+  {
+    "revision": "74405a3875e9c1590bef",
+    "url": "/React-snap-shot/static/js/runtime-main.63b35955.js"
+  }
+]);
